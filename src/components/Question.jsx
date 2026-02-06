f1801fe480463cce26fe5125d6dcf94e516adef0
avina25 <avinaalmeida9@gmail.com>
2026-02-06 11:44:32 +0530

    Displays question text along with progress bar and list of answers

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -50,7 +50,7 @@ export default function Question({ index, onSelectAnswer, onSkipAnswer }) {
         onTimeout={answer.selectedAnswer === '' ? onSkipAnswer : null}
         mode={answerState}
       />
-      <h2>{QUESTIONS[index].text}</h2>
+      
       <Answers
         answers={QUESTIONS[index].answers}
         selectedAnswer={answer.selectedAnswer}
